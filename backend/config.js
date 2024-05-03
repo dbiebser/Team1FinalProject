2e44dacdd90c2478969f7eca18e7df6cfca0ce53
dbiebser <56704481+dbiebser@users.noreply.github.com>
2024-05-03 16:21:02 -0500

    Delete backend directory

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://bieberd1010:password1233@secoms319.xgho7tr.mongodb.net/products-collection?retryWrites=true&w=majority&appName=SECOMS319';
