9587517c57a43ce6624995c931ab89512e752034
dbiebser <56704481+dbiebser@users.noreply.github.com>
2024-05-03 16:19:56 -0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://bieberd1010:password1233@secoms319.xgho7tr.mongodb.net/products-collection?retryWrites=true&w=majority&appName=SECOMS319';
