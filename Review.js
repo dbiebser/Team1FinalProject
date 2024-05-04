ba31bbdd01492b51bccd63989ee2a82df1d76608
dbiebser <56704481+dbiebser@users.noreply.github.com>
2024-05-04 14:01:58 -0500

    Add files via upload

---
+++ Review.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { FaStar } from 'react-icons/fa'
+
+function Review() {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+    return (
+        <div>
+            {[...Array(5)].map((star, index) => {
+                const currentRating = index + 1;
+                return(
+                    <label>
+                        <input type="radio" name="rating" value={currentRating} onClick={() => setRating(currentRating)}/>
+                        <FaStar className='star' size={50} color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                        onMouseEnter={() => setHover(currentRating)} onMouseLeave={() => setHover(null)}/>
+                    </label>
+                );
+            })}
+        </div>
+    )
+}
+
+//Insert next lines in css file
+// input[type=radio]{
+//     display: none;
+// }
+
+// .star{
+//     cursor: pointer;
+// }
